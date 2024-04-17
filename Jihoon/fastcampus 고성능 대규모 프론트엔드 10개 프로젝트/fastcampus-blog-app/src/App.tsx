c22cd9181af7e38c1423f442de421abdf7447734
jihoon kwak <kwakjihoon1001@gmail.com>
2024-04-17 23:47:36 +0900

    CD

--- Jihoon/fastcampus 고성능 대규모 프론트엔드 10개 프로젝트/fastcampus-blog-app/src/App.tsx
+++ Jihoon/fastcampus 고성능 대규모 프론트엔드 10개 프로젝트/fastcampus-blog-app/src/App.tsx
@@ -1,40 +1,11 @@
 import './App.css';
-import { Route, Routes, Navigate, Link} from "react-router-dom";
+import Router from "./components/Router";
 
 function App() {
     return (
-    <>
-    <ul>
-        <li>
-            <Link to="/">Home</Link>
-        </li>
-        <li>
-            <Link to="/posts">post list</Link>
-        </li>
-        <li>
-            <Link to="/posts/:id">post detail</Link>
-        </li>
-        <li>
-            <Link to="/posts/new">post new</Link>
-        </li>
-        <li>
-            <Link to="/posts/edit/:id">post edit</Link>
-        </li>
-        <li>
-            <Link to="/profile">profile</Link>
-        </li>
-    </ul>
-       <Routes>
-        <Route path="/" element={<h1>Home Page</h1>} />
-        <Route path="/posts" element={<h1>Post List Page</h1>} />
-        <Route path="/posts/:id" element={<h1>Post Detail Page</h1>} />
-        <Route path="/posts/new" element={<h1>Post New Page</h1>} />
-        <Route path="/posts/edit/:id" element={<h1>Post Edit Page</h1>} />
-        <Route path="/profile" element={<h1>Profile Page</h1>} />
-        <Route path="*" element={<Navigate replace to="/" />} />
-       </Routes>
-       </>
+    <Router />
     );
 }
 
 export default App;
+
