73d77aed0654d5f6942761c1726a36af5b1f2ab6
jihoon kwak <kwakjihoon1001@gmail.com>
2024-04-18 16:51:49 +0900

    feat: Add components & CSS

--- Jihoon/fastcampus 고성능 대규모 프론트엔드 10개 프로젝트/fastcampus-blog-app/src/App.tsx
+++ Jihoon/fastcampus 고성능 대규모 프론트엔드 10개 프로젝트/fastcampus-blog-app/src/App.tsx
@@ -1,4 +1,3 @@
-import './App.css';
 import Router from "./components/Router";
 
 function App() {
--- Jihoon/fastcampus 고성능 대규모 프론트엔드 10개 프로젝트/fastcampus-blog-app/src/components/Router.tsx
+++ Jihoon/fastcampus 고성능 대규모 프론트엔드 10개 프로젝트/fastcampus-blog-app/src/components/Router.tsx
@@ -1,17 +1,26 @@
-import './App.css';
-import { Route, Routes, Navigate, Link} from "react-router-dom";
+import { Route, Routes, Navigate} from "react-router-dom";
+import Home from "../pages/home";
+import PostList from "../pages/posts";
+import PostDetail from "../pages/posts/detail";
+import Profile from "../pages/profile";
+import PostEdit from "../pages/posts/edit";
+import PostNew from "../pages/posts/new";
+import LoginPage from "../pages/login";
+import SignupPage from "../pages/signup";
 
 export default function Router() {
     return (
     <>
        <Routes>
-        <Route path="/" element={<h1>Home Page</h1>} />
-        <Route path="/posts" element={<h1>Post List Page</h1>} />
-        <Route path="/posts/:id" element={<h1>Post Detail Page</h1>} />
-        <Route path="/posts/new" element={<h1>Post New Page</h1>} />
-        <Route path="/posts/edit/:id" element={<h1>Post Edit Page</h1>} />
-        <Route path="/profile" element={<h1>Profile Page</h1>} />
-        <Route path="*" element={<Navigate replace to="/" />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/posts" element={<PostList />} />
+        <Route path="/posts/:id" element={<PostDetail />} />
+        <Route path="/posts/new" element={<PostNew />} />
+        <Route path="/posts/edit/:id" element={<PostEdit />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignupPage />} />
+        <Route path="*" element={<Navigate replace to="/" />} /> 
        </Routes>
        </>
     );
