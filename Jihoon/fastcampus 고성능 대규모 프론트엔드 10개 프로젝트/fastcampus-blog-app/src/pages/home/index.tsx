3cd4aad6e4af749d6bbc3c953851445679e21722
jihoon kwak <kwakjihoon1001@gmail.com>
2024-04-18 18:43:51 +0900

    feat: Add home(blog main page) components structure

--- Jihoon/fastcampus 고성능 대규모 프론트엔드 10개 프로젝트/fastcampus-blog-app/src/pages/home/index.tsx
+++ Jihoon/fastcampus 고성능 대규모 프론트엔드 10개 프로젝트/fastcampus-blog-app/src/pages/home/index.tsx
@@ -11,11 +11,35 @@ export default function Home() {
                     <Link to="/profile">프로필</Link>
                 </div>
             </header>
-            <div className="post-list">Post List</div>
+            <div className="post__navigation">
+                <div className="post__navigation--active">전체</div>
+                <div>나의 글</div>
+            </div>
+            <div className="post__list">
+                {[...Array(10)].map((e, index) => (
+                    <div key={index} className="post__box">
+                        <Link to={`/posts/${index}`}>
+                            <div className="post__profile-box">
+                                <div className="post__profile"/>
+                                <div className="post__author-name">패스트캠퍼스</div>
+                                <div className="post__date">2024.04.18 목요일</div>
+                            </div>
+                            <div className="post__title">게시글 {index}</div>
+                            <div className="post__text">
+                                하이
+                            </div>
+                            <div className="post__utils-box">
+                                <div className="post__delete">삭제</div>
+                                <div className="post__edit">수정</div>
+                            </div>
+                        </Link>
+                    </div>
+                ))}
+            </div>
             <footer>
-                <div>Menu 1</div>
-                <div>Menu 2</div>
-                <div>Menu 3</div>
+                <Link to="/posts/new">글쓰기</Link>
+                <Link to="/posts">게시글</Link>
+                <Link to="/profile">프로필</Link>
             </footer>
         </div>
     )
