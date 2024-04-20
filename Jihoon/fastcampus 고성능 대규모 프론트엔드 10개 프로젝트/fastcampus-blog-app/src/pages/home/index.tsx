2a8f8b618162bd7d6179b09e48a475f9853f43ef
jihoon kwak <kwakjihoon1001@gmail.com>
2024-04-20 13:27:59 +0900

    Modify components import Route

--- Jihoon/fastcampus 고성능 대규모 프론트엔드 10개 프로젝트/fastcampus-blog-app/src/pages/home/index.tsx
+++ Jihoon/fastcampus 고성능 대규모 프론트엔드 10개 프로젝트/fastcampus-blog-app/src/pages/home/index.tsx
@@ -1,8 +1,6 @@
-import { Link } from "react-router-dom";
-import "../../index.css";
-import Header from "../../components/Header";
-import Footer from "../../components/Footer";
-import PostList from "../../components/PostList";
+import Header from "components/Header";
+import Footer from "components/Footer";
+import PostList from "components/PostList";
 
 export default function Home() {
     return (
