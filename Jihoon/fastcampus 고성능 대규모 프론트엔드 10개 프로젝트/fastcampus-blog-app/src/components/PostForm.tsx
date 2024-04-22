89b09ff4c583aee0cc4f1e692a8e957e9ec8782f
jihoon kwak <kwakjihoon1001@gmail.com>
2024-04-22 20:15:00 +0900

    feat: Add Carousel components

---
+++ Jihoon/fastcampus 고성능 대규모 프론트엔드 10개 프로젝트/fastcampus-blog-app/src/components/PostForm.tsx
@@ -0,0 +1,3 @@
+export default function PostForm() {
+    return <div>post form</div>;
+}
--- Jihoon/fastcampus 고성능 대규모 프론트엔드 10개 프로젝트/fastcampus-blog-app/src/pages/posts/new.tsx
+++ Jihoon/fastcampus 고성능 대규모 프론트엔드 10개 프로젝트/fastcampus-blog-app/src/pages/posts/new.tsx
@@ -1,11 +1,12 @@
 import Footer from "components/Footer";
 import Header from "components/Header";
+import PostForm from "components/PostForm";
 
 export default function PostNew() {
     return (
         <>
         <Header />
-        <div>Post Form</div>
+        <PostForm />
         <Footer />
         </>
     )
