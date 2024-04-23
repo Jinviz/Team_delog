f7fa2ac7f0961af9ec8d669d3d7d7acf2f53091e
jihoon kwak <kwakjihoon1001@gmail.com>
2024-04-23 19:55:10 +0900

    feat: Add login logic based in firebase

--- Jihoon/fastcampus 고성능 대규모 프론트엔드 10개 프로젝트/fastcampus-blog-app/src/components/Router.tsx
+++ Jihoon/fastcampus 고성능 대규모 프론트엔드 10개 프로젝트/fastcampus-blog-app/src/components/Router.tsx
@@ -1,27 +1,39 @@
-import { Route, Routes, Navigate} from "react-router-dom";
-import Home from "../pages/home";
-import PostList from "../pages/posts";
-import PostDetail from "../pages/posts/detail";
-import Profile from "../pages/profile";
-import PostEdit from "../pages/posts/edit";
-import PostNew from "../pages/posts/new";
-import LoginPage from "../pages/login";
-import SignupPage from "../pages/signup";
+import { Route, Routes, Navigate } from "react-router-dom";
+import Home from "pages/home";
+import PostList from "pages/posts";
+import PostDetail from "pages/posts/detail";
+import PostNew from "pages/posts/new";
+import PostEdit from "pages/posts/edit";
+import ProfilePage from "pages/profile";
+import LoginPage from "pages/login";
+import SignupPage from "pages/signup";
 
-export default function Router() {
-    return (
+interface RouterProps {
+  isAuthenticated: boolean;
+}
+
+export default function Router({ isAuthenticated }: RouterProps) {
+  return (
     <>
-       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/posts" element={<PostList />} />
-        <Route path="/posts/:id" element={<PostDetail />} />
-        <Route path="/posts/new" element={<PostNew />} />
-        <Route path="/posts/edit/:id" element={<PostEdit />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/signup" element={<SignupPage />} />
-        <Route path="*" element={<Navigate replace to="/" />} /> 
-       </Routes>
-       </>
-    );
+      <Routes>
+        {isAuthenticated ? (
+          <>
+            <Route path="/" element={<Home />} />
+            <Route path="/posts" element={<PostList />} />
+            <Route path="/posts/:id" element={<PostDetail />} />
+            <Route path="/posts/new" element={<PostNew />} />
+            <Route path="/posts/edit/:id" element={<PostEdit />} />
+            <Route path="/profile" element={<ProfilePage />} />
+            <Route path="*" element={<Navigate replace to="/" />} />
+          </>
+        ) : (
+          <>
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/signup" element={<SignupPage />} />
+            <Route path="*" element={<LoginPage />} />
+          </>
+        )}
+      </Routes>
+    </>
+  );
 }
--- Jihoon/fastcampus 고성능 대규모 프론트엔드 10개 프로젝트/fastcampus-blog-app/src/index.tsx
+++ Jihoon/fastcampus 고성능 대규모 프론트엔드 10개 프로젝트/fastcampus-blog-app/src/index.tsx
@@ -1,16 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import { BrowserRouter as Router } from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { BrowserRouter as Router } from "react-router-dom";
+import { AuthContextProvider } from "context/AuthContext";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
-  <React.StrictMode>
+  <AuthContextProvider>
     <Router>
-    <App />
+      <App />
     </Router>
-  </React.StrictMode>
+  </AuthContextProvider>
 );
