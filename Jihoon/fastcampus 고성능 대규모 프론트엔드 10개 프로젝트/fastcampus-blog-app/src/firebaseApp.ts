2a02e6b4cc1ce2e9761312a368c29ed8f75618de
jihoon kwak <kwakjihoon1001@gmail.com>
2024-04-23 23:25:25 +0900

    modify: modify .env Route

--- Jihoon/fastcampus 고성능 대규모 프론트엔드 10개 프로젝트/fastcampus-blog-app/src/firebaseApp.ts
+++ Jihoon/fastcampus 고성능 대규모 프론트엔드 10개 프로젝트/fastcampus-blog-app/src/firebaseApp.ts
@@ -1,7 +1,7 @@
 import { initializeApp, FirebaseApp, getApp } from "firebase/app";
 import "firebase/auth";
 
-export let app: FirebaseApp;
+export let app: FirebaseApp; //지역변수로 할당
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -17,7 +17,8 @@ try {
   app = getApp("app");
 } catch (e) {
   app = initializeApp(firebaseConfig, "app");
-}
+} //기존의 앱을 가져오고, 그게 아니라면 초기화 된 앱을 가져오라는 뜻
+//매번 initialize를 하면 비효율적이기 때문이다.
 
 // Initialize Firebase
 const firebase = initializeApp(firebaseConfig);
