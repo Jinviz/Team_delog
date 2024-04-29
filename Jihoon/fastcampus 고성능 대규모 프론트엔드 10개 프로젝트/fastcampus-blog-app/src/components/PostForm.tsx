95851ed2d5044d811b1a490c47704167ce5bc7e3
jihoon135 <kwakjihoon1001@gmail.com>
2024-04-29 19:08:27 +0900

    firestore logic -> modify postlist, postform, postdetail

--- Jihoon/fastcampus 고성능 대규모 프론트엔드 10개 프로젝트/fastcampus-blog-app/src/components/PostForm.tsx
+++ Jihoon/fastcampus 고성능 대규모 프론트엔드 10개 프로젝트/fastcampus-blog-app/src/components/PostForm.tsx
@@ -1,44 +1,96 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
+import { collection, addDoc } from "firebase/firestore";
+import { db } from "firebaseApp";
+import AuthContext from "context/AuthContext";
+
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
 
 export default function PostForm() {
   const [title, setTitle] = useState<string>("");
   const [summary, setSummary] = useState<string>("");
   const [content, setContent] = useState<string>("");
+  const { user } = useContext(AuthContext);
+  const navigate = useNavigate();
 
-  const onChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
-    const { target: { name, value },
-  } = e;
+  const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
 
-  if (name === "title") {
-    setTitle(value);
-  }
+    try {
+      // firestore로 데이터 생성
+      await addDoc(collection(db, "posts"), {
+        title: title,
+        summary: summary,
+        content: content,
+        createAt: new Date()?.toLocaleDateString(),
+        email: user?.email,
+      });
 
-  if (name === "summary") {
-    setSummary(value);
-  }
+      toast?.success("게시글을 생성했습니다.");
+      navigate("/");
+    } catch (e: any) {
+      console.log(e);
+      toast?.error(e?.code);
+    }
+  };
 
-  if (name === "content") {
-    setContent(value);
-  }
-  }
+  const onChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    const {
+      target: { name, value },
+    } = e;
 
-    return (
-      <form action="/post" method="POST" className="form">
-        <div className="form__block">
-          <label htmlFor="title">제목</label>
-          <input type="text" name="title" id="title" required />
-        </div>
-        <div className="form__block">
-          <label htmlFor="summary">요약</label>
-          <input type="text" name="summary" id="summary" required />
-        </div>
-        <div className="form__block">
-          <label htmlFor="content">내용</label>
-          <textarea name="content" id="content" required />
-        </div>
-        <div className="form__block">
-          <input type="submit" value="제출" className="form__btn--submit" />
-        </div>
-      </form>
-    );
-  }
+    if (name === "title") {
+      setTitle(value);
+    }
+
+    if (name === "summary") {
+      setSummary(value);
+    }
+
+    if (name === "content") {
+      setContent(value);
+    }
+  };
+
+  return (
+    <form onSubmit={onSubmit} className="form">
+      <div className="form__block">
+        <label htmlFor="title">제목</label>
+        <input
+          type="text"
+          name="title"
+          id="title"
+          required
+          onChange={onChange}
+          value={title}
+        />
+      </div>
+      <div className="form__block">
+        <label htmlFor="summary">요약</label>
+        <input
+          type="text"
+          name="summary"
+          id="summary"
+          required
+          onChange={onChange}
+          value={summary}
+        />
+      </div>
+      <div className="form__block">
+        <label htmlFor="content">내용</label>
+        <textarea
+          name="content"
+          id="content"
+          required
+          onChange={onChange}
+          value={content}
+        />
+      </div>
+      <div className="form__block">
+        <input type="submit" value="제출" className="form__btn--submit" />
+      </div>
+    </form>
+  );
+}
--- Jihoon/fastcampus 고성능 대규모 프론트엔드 10개 프로젝트/fastcampus-blog-app/src/components/PostList.tsx
+++ Jihoon/fastcampus 고성능 대규모 프론트엔드 10개 프로젝트/fastcampus-blog-app/src/components/PostList.tsx
@@ -1,58 +1,89 @@
-import { useState } from "react";
+import AuthContext from "context/AuthContext";
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "firebaseApp";
+import { useContext, useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 interface PostListProps {
-    hasNavigation?: boolean;
-} //PostList 컴포넌트가 받을 수 있는 props 타입 정의
+  hasNavigation?: boolean;
+}
 
 type TabType = "all" | "my";
 
-export default function PostList({ hasNavigation = true}: PostListProps) {
-    const [activeTab, setActiveTab] = useState<TabType>("all");
-    //hasNavigation의 기본값은 true, 별도의 prop이 제공되지 않으면 true다.
-    return (
-        <> 
-        {hasNavigation && ( //hasNavigation이 true일 때만 이 식이 렌더링
-        // 왼쪽 피연산자(hasNavigation 여기서)의 값이 참 같은 값(truthy)일 때 
-        //오른쪽 피연산자(여기서는 (...))의 값을 반환
+export interface PostProps {
+  id?: string;
+  title: string;
+  email: string;
+  summary: string;
+  content: string;
+  createdAt: string;
+}
+
+export default function PostList({ hasNavigation = true }: PostListProps) {
+  const [activeTab, setActiveTab] = useState<TabType>("all");
+  const [posts, setPosts] = useState<PostProps[]>([]);
+  const { user } = useContext(AuthContext);
+
+  const getPosts = async () => {
+    const datas = await getDocs(collection(db, "posts"));
+
+    datas?.forEach((doc) => {
+      const dataObj = { ...doc.data(), id: doc.id };
+      setPosts((prev) => [...prev, dataObj as PostProps]);
+    });
+  };
+
+  useEffect(() => {
+    getPosts();
+  }, []);
+
+  return (
+    <>
+      {hasNavigation && (
         <div className="post__navigation">
-        <div
-          role="presentation"
-          onClick={() => setActiveTab("all")}
-          className={activeTab === "all" ? "post__navigation--active" : ""}
-        >
-          전체
+          <div
+            role="presentation"
+            onClick={() => setActiveTab("all")}
+            className={activeTab === "all" ? "post__navigation--active" : ""}
+          >
+            전체
+          </div>
+          <div
+            role="presentation"
+            onClick={() => setActiveTab("my")}
+            className={activeTab === "my" ? "post__navigation--active" : ""}
+          >
+            나의 글
+          </div>
         </div>
-        <div
-          role="presentation"
-          onClick={() => setActiveTab("my")}
-          className={activeTab === "my" ? "post__navigation--active" : ""}
-        >
-          나의 글
-        </div>
-      </div>
-    )}
-        <div className="post__list">
-                {[...Array(10)].map((e, index) => (
-                    <div key={index} className="post__box">
-                        <Link to={`/posts/${index}`}>
-                            <div className="post__profile-box">
-                                <div className="post__profile"/>
-                                <div className="post__author-name">패스트캠퍼스</div>
-                                <div className="post__date">2024.04.18 목요일</div>
-                            </div>
-                            <div className="post__title">게시글 {index}</div>
-                            <div className="post__text">
-                                하이
-                            </div>
-                            <div className="post__utils-box">
-                                <div className="post__delete">삭제</div>
-                                <div className="post__edit">수정</div>
-                            </div>
-                        </Link>
-                    </div>
-                ))}
+      )}
+      <div className="post__list">
+        {posts?.length > 0 ? (
+          posts?.map((post, index) => (
+            <div key={post?.id} className="post__box">
+              <Link to={`/posts/${post?.id}`}>
+                <div className="post__profile-box">
+                  <div className="post__profile" />
+                  <div className="post__author-name">{post?.email}</div>
+                  <div className="post__date">{post?.createdAt}</div>
+                </div>
+                <div className="post__title">{post?.title}</div>
+                <div className="post__text">{post?.summary}</div>
+              </Link>
+              {post?.email === user?.email && (
+                <div className="post__utils-box">
+                  <div className="post__delete">삭제</div>
+                  <Link to={`/posts/edit/${post?.id}`} className="post__edit">
+                    수정
+                  </Link>
+                </div>
+              )}
             </div>
-        </>
-    )
+          ))
+        ) : (
+          <div className="post__no-post">게시글이 없습니다.</div>
+        )}
+      </div>
+    </>
+  );
 }
