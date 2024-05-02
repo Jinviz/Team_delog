4db2f9c7de4887510ad7cbb6e3024b7f8f49f1df
minggichae <che08180@naver.com>
2024-05-02 11:51:20 +0900

    feat: dark mode

---
+++ Minggi/fastcampus - 10개의 프로젝트/src/context/ThemeContext.tsx
@@ -0,0 +1,30 @@
+import { ReactNode, createContext, useState } from "react";
+
+const ThemeContext = createContext({
+  theme: "light",
+  toggleMode: () => {},
+});
+
+interface ThemeProps {
+  children: ReactNode;
+}
+
+export const ThemeContextProvider = ({ children }: ThemeProps) => {
+  const [theme, setTheme] = useState(
+    // 창을 닫거나, 새로 고침을 해도 그 전의 상태를 window에 저장하기 위한 코드
+    window.localStorage.getItem("theme") || "light"
+  );
+
+  const toggleMode = () => {
+    setTheme((prev) => (prev === "light" ? "dark" : "light"));
+    window.localStorage.setItem("theme", theme === "light" ? "dark" : "light");
+  };
+
+  return (
+    <ThemeContext.Provider value={{ theme, toggleMode }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
+
+export default ThemeContext;
