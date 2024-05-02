440986acfef9f4a2bd451b73a1a4e7febec19338
민기 <che08180@naver.com>
2024-05-02 14:47:32 +0900

    modify: file, feat: dark mode commit

--- Minggi/fastcampus - 10개의 프로젝트/src/App.tsx
+++ Minggi/fastcampus - 10개의 프로젝트/src/App.tsx
@@ -1,13 +1,15 @@
-import { useEffect, useState } from "react";
-import { app} from "firebaseAPP";
+import { useEffect, useState, useContext } from "react";
+import { app } from "firebaseAPP";
 import { getAuth, onAuthStateChanged } from "firebase/auth"; // 현재 firbase의 사용자 로그인 여부 체크
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import ThemeContext from "context/ThemeContext";
 
 import Router from "./components/Router";
 import Loader from "components/Loader";
 
 function App() {
+  const context = useContext(ThemeContext);
   const auth = getAuth(app); // getAuth는 app을 넣어야 작동함.
   // auth를 체크하기 전에 (initialize 전)에는 loader를 띄워주는 용도
   const [init, setInit] = useState<boolean>(false);
@@ -30,10 +32,10 @@ function App() {
   }, [auth]);
 
   return (
-    <>
+    <div className={context.theme === "light" ? "white" : "dark"}>
       <ToastContainer />
       {init ? <Router isAuthenticated={isAuthenticated} /> : <Loader />}
-    </>
+    </div>
   );
 }
 
