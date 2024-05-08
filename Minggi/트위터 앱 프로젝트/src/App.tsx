f97ba91007b29467b27c8b2dd33fd7cf3a3894f6
minggichae <che08180@naver.com>
2024-05-09 01:03:20 +0900

    feat: directory settings

---
+++ Minggi/트위터 앱 프로젝트/src/App.tsx
@@ -0,0 +1,11 @@
+import HomePage from "pages/home";
+import "./App.css";
+
+import { Route, Routes, Navigate } from "react-router-dom";
+import Router from "components/Router";
+
+function App() {
+  return <Router />;
+}
+
+export default App;
