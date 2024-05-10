7d25abdc594e5af2af05b9d6d989c8c64e8056b6
minggichae <che08180@naver.com>
2024-05-10 21:24:35 +0900

    feat: FirebaseAuth Settings

--- Minggi/트위터 앱 프로젝트/src/App.tsx
+++ Minggi/트위터 앱 프로젝트/src/App.tsx
@@ -1,7 +1,17 @@
+import { useState } from "react";
+
 import Router from "components/Router";
 import { Layout } from "components/Layout";
 
+import { getAuth } from "firebase/auth";
+import { app } from "firebaseApp";
+
 function App() {
+  const auth = getAuth(app);
+  const [isAuthenticated, setIsAuthenticated] = useState<boolean>(
+    !!auth?.currentUser
+  );
+
   return (
     <Layout>
       <Router />
--- Minggi/트위터 앱 프로젝트/src/pages/home/index.tsx
+++ Minggi/트위터 앱 프로젝트/src/pages/home/index.tsx
@@ -1,8 +1,5 @@
-import { FiImage } from "react-icons/fi";
-import { FaUserCircle } from "react-icons/fa";
-import { Link } from "react-router-dom";
-import { AiFillHeart } from "react-icons/ai";
-import { FaRegComment } from "react-icons/fa";
+import PostForm from "components/posts/PostForm";
+import PostBox from "components/posts/PostBox";
 
 export interface PostProps {
   id: string;
@@ -20,98 +17,59 @@ const posts: PostProps[] = [
   {
     id: "1",
     email: "test@test.com",
-    content: "내용입니다.",
-    createdAt: "2024-05-09",
+    content: "내용입니다",
+    createdAt: "2023-08-30",
+    uid: "123123",
+  },
+  {
+    id: "2",
+    email: "test@test.com",
+    content: "내용입니다",
+    createdAt: "2023-08-30",
+    uid: "123123",
+  },
+  {
+    id: "3",
+    email: "test@test.com",
+    content: "내용입니다",
+    createdAt: "2023-08-30",
+    uid: "123123",
+  },
+  {
+    id: "4",
+    email: "test@test.com",
+    content: "내용입니다",
+    createdAt: "2023-08-30",
+    uid: "123123",
+  },
+  {
+    id: "5",
+    email: "test@test.com",
+    content: "내용입니다",
+    createdAt: "2023-08-30",
+    uid: "123123",
+  },
+  {
+    id: "6",
+    email: "test@test.com",
+    content: "내용입니다",
+    createdAt: "2023-08-30",
     uid: "123123",
   },
 ];
 
 export default function HomePage() {
-  const handleFileUpload = () => {};
-
-  const handleDelete = () => {};
-
   return (
     <div className="home">
-      <div className="home__title"></div>
+      <div className="home__title">Home</div>
       <div className="home__tabs">
         <div className="home__tab home__tab--active">For You</div>
-        <div className="home__tab">Follwing</div>
+        <div className="home__tab">Following</div>
       </div>
-      {/* Post Form */}
-      <form className="post-form">
-        <textarea
-          className="post-from__textarea"
-          required
-          name="content"
-          id="content"
-          placeholder="What is happening?"
-        />
-        <div className="post-form__submit-area">
-          <label htmlFor="file-input" className="post-form__file">
-            <FiImage className="post-form__file-icon" />
-          </label>
-          <input
-            type="file"
-            name="file-input"
-            accept="image/*"
-            onChange={handleFileUpload}
-            className="hidden"
-          />
-          <input
-            type="submit"
-            value={"Tweet"}
-            className="post-form__submit-btn"
-          />
-        </div>
-      </form>
-      {/* Tweet posts */}
+      <PostForm />
       <div className="post">
         {posts?.map((post) => (
-          <div className="post__box" key={post?.id}>
-            <Link to={`/post/${post?.id}`}>
-              <div className="post__box-profile">
-                <div className="post__flex">
-                  {post?.profileUrl ? (
-                    <img
-                      src={post?.profileUrl}
-                      alt="profile" // 보여줄 이미지가 없을 때 이미지를 대체할 텍스트 명
-                      className="post__box-profile-img"
-                    />
-                  ) : (
-                    <FaUserCircle className="post__box-profile-icon" />
-                  )}
-                  <div className="post__email">{post?.email}</div>
-                  <div className="post__createdAt">{post?.createdAt}</div>
-                </div>
-                <div className="post__box-content">{post?.content}</div>
-              </div>
-            </Link>
-            <div className="post__box-footer">
-              {/* post.uid === user.uid 일 때 */}
-              <>
-                <button
-                  type="button"
-                  className="post__delete"
-                  onClick={handleDelete}
-                >
-                  Delete
-                </button>
-                <button type="button" className="post__edit">
-                  <Link to={`/posts/edit/${post?.id}`}></Link>
-                  Edit
-                </button>
-                <button type="button" className="post__likes">
-                  <AiFillHeart />
-                  {post?.likeCount || 0}
-                </button>
-                <button type="button" className="post__comments">
-                  <FaRegComment />
-                  {post?.comments?.length || 0}
-                </button>
-              </>
-            </div>
-          </div>
+          <PostBox post={post} key={post.id} />
         ))}
       </div>
     </div>
